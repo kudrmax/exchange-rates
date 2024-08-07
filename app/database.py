140c3d45d86a5259304bd51584b52fdb7d70e3da
kudrmax <mdkudryashov@edu.hse.ru>
2024-08-07 17:32:40 +0300

    Fix session maker

--- app/database.py
+++ app/database.py
@@ -1,3 +1,7 @@
+from contextlib import contextmanager
+from typing import AsyncIterator
+
+from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
@@ -10,12 +14,33 @@ DB_PASSWORD = 'postgres'
 DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
 engine = create_async_engine(DATABASE_URL)
-
-async_session_maker = sessionmaker(
+async_session = sessionmaker(
     engine,
+    expire_on_commit=False,
     class_=AsyncSession,
-    expire_on_commit=False
 )
+
+
+async def get_session() -> AsyncIterator[AsyncSession]:
+    async with async_session() as session:
+        yield session
+
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
+# Base.metadata.create_all(bind=engine)
+
+# SessionLocal = sessionmaker(
+#     engine,
+#     expire_on_commit=False,
+#     class_=AsyncSession,
+# )
+# def get_session():
+#     session = SessionLocal()
+#     try:
+#         yield session
+#     finally:
+#         session.close()
 
 
 class Base(DeclarativeBase):
