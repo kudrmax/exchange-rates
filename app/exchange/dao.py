157ac021e5bfa7e8633c8ded514b9139c9e28cee
kudrmax <mdkudryashov@edu.hse.ru>
2024-08-08 22:18:11 +0300

    Change exchange function from USD to EUR

--- app/exchange/dao.py
+++ app/exchange/dao.py
@@ -8,17 +8,6 @@ from app.exchange_rates.router import get_one_or_none_exchange_rate
 class DAOExchange:
 
     @classmethod
-    # async def get_pair_schema(cls, base_code, target_code, session):
-    #     base_currency, target_currency = get_one_or_none_exchange_rate(base_code, target_code)
-    #     return SExchange(
-    #         base_currency = SCurrency(**base_currency.__dict__),
-    #         target_currency = SCurrency(**target_currency.__dict__),
-    #         rate = None,
-    #         amount = None,
-    #         converted_amount = None
-    #     )
-
-    @classmethod
     async def exchange(cls, code_from: str, code_to: str, amount: float, session):
         pair = await get_one_or_none_exchange_rate(code_from + code_to, session)
 
@@ -43,27 +32,27 @@ class DAOExchange:
                 converted_amount=amount / pair.rate
             )
 
-        # есть курс А-USD или USD-A
-        rate_from_usd = None
-        pair_from_usd = await get_one_or_none_exchange_rate(code_from + 'USD', session)
-        if pair_from_usd:
-            rate_from_usd = pair_from_usd.rate
+        # в БД нет ни FT, ни TF, но есть курс F-EUR или EUR-T или T-EUR или EUR-F
+        rate_from_eur = None
+        pair_from_eur = await get_one_or_none_exchange_rate(code_from + 'EUR', session)
+        if pair_from_eur:
+            rate_from_eur = pair_from_eur.rate
         else:
-            pair_usd_from = await get_one_or_none_exchange_rate('USD' + code_from, session)
-            if pair_usd_from:
-                rate_from_usd = 1 / pair_usd_from.rate
+            pair_eur_from = await get_one_or_none_exchange_rate('EUR' + code_from, session)
+            if pair_eur_from:
+                rate_from_eur = 1 / pair_eur_from.rate
 
-        rate_to_usd = None
-        pair_to_usd = await get_one_or_none_exchange_rate(code_to + 'USD', session)
-        if pair_to_usd:
-            rate_to_usd = pair_to_usd.rate
+        rate_to_eur = None
+        pair_to_eur = await get_one_or_none_exchange_rate(code_to + 'EUR', session)
+        if pair_to_eur:
+            rate_to_eur = pair_to_eur.rate
         else:
-            pair_usd_to = await get_one_or_none_exchange_rate('USD' + code_to, session)
-            if pair_usd_to:
-                rate_to_usd = 1 / pair_usd_to.rate
+            pair_eur_to = await get_one_or_none_exchange_rate('EUR' + code_to, session)
+            if pair_eur_to:
+                rate_to_eur = 1 / pair_eur_to.rate
 
-        if rate_from_usd and rate_to_usd:
-            rate = rate_from_usd / rate_to_usd
+        if rate_from_eur and rate_to_eur:
+            rate = rate_from_eur / rate_to_eur
             base_currency = (await get_one_or_none_currencies(code_from, session)).__dict__
             target_currency = (await get_one_or_none_currencies(code_to, session)).__dict__
             return SExchange(
